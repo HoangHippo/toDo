15ef6ceb4b4b43331433e41b9cdc9da6f040c97b
HoangHippo <hoanghippo17@gmail.com>
2021-12-12 22:25:15 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import clsx from "clsx";
+
+import "./App.css";
 
 function App() {
+  const [text, setText] = React.useState("");
+
+  const [tasks, setTasks] = React.useState(() => {
+    const storedTasks = localStorage.getItem("tasks");
+
+    if (!storedTasks) {
+      return [];
+    }
+
+    return JSON.parse(storedTasks);
+  });
+
+  const handleAddTasks = () => {
+    if (text) {
+      setTasks((preTasks) => [
+        ...preTasks,
+        { content: text, isCompleted: false },
+      ]
+      );
+      setText('');
+    }
+  };
+
+  const handleChangeInput = (e) => {
+    const value = e.target.value;
+    setText(value);
+  };
+
+  const clsList = clsx({
+    border: tasks.length > 0,
+    "divide-y divide-grey-100 mt-8": true,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container mx-auto">
+      <div className="mt-3 flex">
+        <div className="flex-grow">
+          <input
+            className="w-full h-full px-2 bordered rounded"
+            value={text}
+            placeholder="Enter new task"
+            onChange={handleChangeInput}
+          ></input>
+        </div>
+        <button
+          className="px-2 py-3 text-white bg-blue-500"
+          onKeyPress={(e) => {
+            if (e.key === "Enter") {
+              handleAddTasks();
+            }
+          }}
+          onClick={handleAddTasks}
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </div>
+      <ul className={clsList}>
+        {tasks.map((task, idx) => {
+          return (
+            <li className="p-2 flex">
+              <div className="mr-2 incline-flex items-center flex-grow">
+                {task.content}
+              </div>
+              <button className="px-2 py-3 text-white bg-red-500">
+                Delete
+              </button>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
